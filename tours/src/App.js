913914605fb667a28786efb57294ead281b2137a
Krunal Darji <krunalkdarji@gmail.com>
2020-12-24 23:09:32 -0500

    #2: Tours listing project completed

---
+++ tours/src/App.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+
+const url = "https://course-api.com/react-tours-project";
+
+function App() {
+  const [loading, setloading] = useState(true);
+  const [tours, settours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    settours(newTours);
+  };
+
+  const fetchTours = async () => {
+    setloading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setloading(false);
+      settours(tours);
+    } catch (error) {
+      setloading(false);
+      console.log(error);
+    }
+
+    console.log(tours);
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
+}
+
+export default App;
