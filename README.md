8a14feb7001590ca71bfa8b53887efa7babe894f
Krunal Darji <krunalkdarji@gmail.com>
2021-01-24 18:12:42 -0500

    #15: cocktails completed

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-REACT Projects: https://youtu.be/a_7Z7C_JCyo
+React Projects tutorial by John Smilga: https://youtu.be/a_7Z7C_JCyo
 
-1. Birthday Reminder:
+1. Birthday-reminder
+2. Tours
+3. Reviews
+4. Accordion
+5. Menu
+6. Tabs
+7. Slider
+8. Lorem-ipsum
+9. Color-generator
+10. Grocery-bud
+11. Navbar
+12. Sidebar
+13. Stripe-submenus
+14. Cart
+15. Cocktails
---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+        reprehenderit veniam laborum delectus accusamus repellendus porro quia
+        blanditiis excepturi, dolorem dignissimos quis at optio esse cupiditate
+        corporis, molestias ipsum quos quisquam nihil temporibus possimus!
+        Reiciendis facilis itaque sed voluptatibus dolor quam architecto quod
+        iste!
+      </p>
+    </section>
+  );
+};
+
+export default About;
